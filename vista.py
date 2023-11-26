8d8f043cf9abbc3281a98bbf6a668525f9066f45
Laura00Ag <laura.acevedog@udea.edu.co>
2023-11-25 23:16:08 -0500

    docs

--- vista.py
+++ vista.py
@@ -1,29 +1,37 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QSlider
 from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUi
+
 import os
 
 class Vista(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi('base.ui', self)
-    
+        self.slider = QSlider(self)
+        self.slider.setGeometry(10, 400, 380, 20)  # Ajusta la geometría según tus necesidades
+        self.slider.setOrientation(0x1)  # Configura la orientación horizontal
+
     def setup(self):
         self.comboBox.currentIndexChanged.connect(self.cargar)
+        self.slider.valueChanged.connect(self.cargar)
+
         self.carpeta = 'images'
         lista_archivos = os.listdir(self.carpeta)
-        #self.slider.valueChanged.connect(self.cargar)
-        #self.current_index = self.slider.value() - 1
+        self.slider.setMaximum(len(lista_archivos) - 1)  # Establece el valor máximo del slider
+
         for archivo in lista_archivos:
             self.comboBox.addItem(archivo)
-    
-    def addControler(self,c):
-        self.__mi_coordinador=c
+
+    def addControler(self, c):
+        self.__mi_coordinador = c
         self.setup()
 
     def cargar(self):
-        imagen = self.comboBox.currentText()
+        index = self.slider.value()
+        imagen = self.comboBox.itemText(index)
         self.__mi_coordinador.img_conextion(imagen)
+
         pixmap = QPixmap("temp_image.png")
         self.img.setPixmap(pixmap)
         os.remove('temp_image.png')
